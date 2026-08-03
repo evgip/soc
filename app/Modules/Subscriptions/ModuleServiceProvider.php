af6dd645b54cfdafb06368a04a18f0269d347585
Evg <budo@narod.ru>
2026-08-03 14:23:59 +0300

    Update ModuleServiceProvider.php

--- app/Modules/Subscriptions/ModuleServiceProvider.php
+++ app/Modules/Subscriptions/ModuleServiceProvider.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Modules\Subscriptions;
 
-use W3a\Core\Container\Container;
+use W3a\Core\Foundation\Container;
 use W3a\Core\Contracts\DatabaseInterface;
 use App\Modules\Subscriptions\Models\FollowedUser;
 use App\Modules\Subscriptions\Models\FollowedTag;
